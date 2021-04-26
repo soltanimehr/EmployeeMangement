dc30f16e2bf75ed837446db4c9f571b3be89cf49
soltanimehr <52445042+soltanimehr@users.noreply.github.com>
2021-04-26 23:44:37 +0200

    Delete Backends/Company-Service/src directory

--- Backends/Company-Service/src/main/java/com/mydomain/company/repository/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mydomain.company.repository;
-
-import com.mydomain.company.entity.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompanyRepository extends JpaRepository<Company,Long> {
-}
--- Backends/Company-Service/src/main/java/com/mydomain/company/service/CompanyService.java
+++
@@ -1,6 +0,0 @@
-package com.mydomain.company.service;
-
-import com.mydomain.company.entity.Company;
-
-public interface CompanyService extends IService<Company> {
-}
